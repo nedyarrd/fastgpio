ed7a100ccdd1fbf449a9bd1fa70fef5ce962216b
Nedyarrd <xperience@interia.pl>
2013-11-02 21:33:23 +0100

    beginning of ioctl

---
+++ fastgpio_ioctl.h
@@ -0,0 +1,10 @@
+#include "fastgpio.h"
+typedef struct
+	{
+	int number;
+	int pins[MAX_GPIO];
+	} gpio_ioctl;
+// which pins We will set
+#define FASTGPIO_SET_PINS _IOW('f',1, gpio_ioctl *);
+// which pins We will read
+#define FASTGPIO_READ_PINS _IOW('f',2, gpio_ioctl *);
