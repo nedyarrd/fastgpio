aed59cb6c4da77851545ba7971ea9229b6deb215
nedyarrd <xperience@interia.pl>
2013-11-07 08:17:14 +0100

    New IOCTL for setting pins direction, without using sysfs exports from userspace

--- fastgpio_ioctl.h
+++ fastgpio_ioctl.h
@@ -3,8 +3,12 @@ typedef struct
 	{
 	int number;
 	int pins[MAX_GPIO];
+	int dir[MAX_GPIO];
 	} gpio_ioctl;
 // which pins We will set
+// caution it defaults that pins are set to out direction
 #define FASTGPIO_SET_PINS _IOW('f',1, gpio_ioctl *)
 // which pins We will read
+// caution this don't set direction of pins, because we can read state of in or out pins
 #define FASTGPIO_READ_PINS _IOW('f',2, gpio_ioctl *)
+#define FASTGPIO_SET_DIR _IOW('f',3,gpio_ioctl *)
